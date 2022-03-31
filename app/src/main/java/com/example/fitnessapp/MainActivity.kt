daca790c808a6ba22523f60539e9a52688c62691
6001965 <adrian.sanz@viewnext.com>
2022-03-31 12:35:37 +0200

    Inicio

--- app/src/main/java/com/example/fitnessapp/MainActivity.kt
+++ app/src/main/java/com/example/fitnessapp/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.fitnessapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.text.SpannableString
+import android.text.style.UnderlineSpan
+
+
+
+
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val gg:TextView= findViewById(R.id.pregunta)
+
+        val mitextoU = SpannableString("¿No tienes cuenta en FitnessLife aùn? Crear cuenta")
+        mitextoU.setSpan(UnderlineSpan(), 0, mitextoU.length, 0)
+        gg.setText(mitextoU)
+
+
+
+        gg.setOnClickListener {
+            val clase=Intent(this,Registro::class.java)
+            startActivity(clase)
+        }
+
     }
 }
