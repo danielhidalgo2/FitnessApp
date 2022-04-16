2b77ebfb723f74663da3a44e2bc6a29a9bc674e3
6001965 <adrian.sanz@viewnext.com>
2022-04-16 19:15:52 +0200

    Inicio

--- app/src/main/java/com/example/fitnessapp/Perfil.kt
+++ app/src/main/java/com/example/fitnessapp/Perfil.kt
@@ -3,10 +3,14 @@ package com.example.fitnessapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Registro : AppCompatActivity() {
+class Perfil : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registro)
+        setContentView(R.layout.activity_perfil)
+
+
+
 
     }
+
 }
