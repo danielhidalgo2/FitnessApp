31a33147795b5f1b9c1de41469f8e72adf19248b
DanielHidalgoViewnext <daniel.hidalgo@viewnext.com>
2022-05-12 11:54:43 +0200

    Implementacion db

--- app/src/main/java/com/example/fitnessapp/ui/view/Ajustes.kt
+++ app/src/main/java/com/example/fitnessapp/ui/view/Ajustes.kt
@@ -3,15 +3,24 @@ package com.example.fitnessapp.ui.view
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import com.example.fitnessapp.R
 import com.example.fitnessapp.databinding.ActivityAjustesBinding
 import com.example.fitnessapp.databinding.ActivityHomeBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
 
 class Ajustes : AppCompatActivity() {
 
+    //private val db = FirebaseFirestore.getInstance()
+    val registro = Registro()
+
     private lateinit var binding: ActivityAjustesBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ajustes)
         binding = ActivityAjustesBinding.inflate(layoutInflater)
@@ -27,6 +36,12 @@ class Ajustes : AppCompatActivity() {
             val  intent = Intent(this,MainActivity::class.java)
             startActivity(intent)
         }
+
+        /*binding.eliminarcuenta.setOnClickListener{
+            db.collection("Users").document(registro.).delete()
+            /*val  intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)*/
+        }*/
     }
 
 
--- app/src/main/java/com/example/fitnessapp/ui/view/Registro.kt
+++ app/src/main/java/com/example/fitnessapp/ui/view/Registro.kt
@@ -4,28 +4,42 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
 import android.widget.Toast
 import com.example.fitnessapp.R
 import com.example.fitnessapp.databinding.ActivityPerfilBinding
 import com.example.fitnessapp.databinding.ActivityRegistroBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
 
 
 enum class ProviderType{
     BASIC
 }
-class Registro : AppCompatActivity() {
+ class Registro : AppCompatActivity() {
+
+    private val db = FirebaseFirestore.getInstance()
 
     private lateinit var binding: ActivityRegistroBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegistroBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-
-
+        var nombreRegistro: EditText= findViewById(R.id.nombreRegistro)
+        var apellidosRegistro: EditText= findViewById(R.id.apellidosRegistro)
+        var correoRegistro: EditText= findViewById(R.id.correoRegistro)
+        var contraseniaRegistro: EditText= findViewById(R.id.contraseñaRegistro)
+        var contraseniaConfirmada: EditText= findViewById(R.id.contraseñaconfirmadaRegistro)
         val boton :Button=findViewById(R.id.registrar)
+
+
 
        /* boton.setOnClickListener {
             val intent =Intent(this,Perfil::class.java)
@@ -35,6 +49,9 @@ class Registro : AppCompatActivity() {
 
         binding.registrar.setOnClickListener {
             if(binding.contraseARegistro.text.toString().isNotEmpty() &&  binding.correoRegistro.text.toString().isNotEmpty()){
+                db.collection("Users").document(correoRegistro.text.toString()).set(
+                    hashMapOf("nombre" to nombreRegistro.text.toString(), "apellido" to apellidosRegistro.text.toString(), "contrasenia" to contraseniaRegistro.text.toString(), "confirmacion" to contraseniaConfirmada.text.toString())
+                )
                 FirebaseAuth.getInstance().createUserWithEmailAndPassword(binding.correoRegistro.text.toString(),binding.contraseARegistro.text.toString()).addOnCompleteListener {
                     if(it.isSuccessful){
                        showHome(it.result?.user?.email ?:"",ProviderType.BASIC)
@@ -47,6 +64,10 @@ class Registro : AppCompatActivity() {
 
             }
         }
+
+        /*fun getCorreo(): String {
+            return correoRegistro.text.toString()
+        }*/
 
 
     }
