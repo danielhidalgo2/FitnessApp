9e22929a19365bc9b74dfb6ccb7d72be010ace1c
Daniel Hidalgo <daniel.ma43476@cesurformacion.com>
2022-06-13 17:11:54 +0200

    splashscreen

---
+++ app/src/main/java/com/example/fitnessapp/ui/view/SplashScreen.kt
@@ -0,0 +1,21 @@
+package com.example.fitnessapp.ui.view
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.example.fitnessapp.R
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+           val intent = Intent(this@SplashScreen, MainActivity::class.java)
+            startActivity(intent)
+        },3000)
+    }
+}
